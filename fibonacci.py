db18ccbb37474b56ef514c7cca2492da12cc1ed8
dantevangelista <76470174+dantevangelista@users.noreply.github.com>
2024-06-07 16:59:09 -0400

    fibonacci general formula

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,13 @@
 # Solves power summation of the Fibonacci sequence
 # F(n) = 1, 1, 2, 3, 5, ...
 
+# Let F_1 = F_2 = 1
+# let N, n in *N*, m in *R*
+# F = sum^N_{n = 1} f^m_n
+# where by Binet's formula, 
+# f_n = 1/sqrt{5} (((1 + sqrt{5})/2)^n - (1 - sqrt{5})/2)^n)
+
+
 F1 = 1
 F2 = 1
 m = 2
